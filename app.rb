9c8ae75e8ddd8b5c71c06bbf263d6e90496e0764
Jesse Wenger <wengerj@gmail.com>
2021-04-07 16:27:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    session[:item] = params[:item].fetch("name")
+    session[:item] = "socks" #params[:item].fetch("name")
     @session = session
     @item = Item.new(params[:item])
     binding.pry
