394c41be0fbc554e9b00afe8237ed571c02de3d1
Jesse Wenger <wengerj@gmail.com>
2021-04-07 16:20:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    session[:item] = "socks"
+    session[:item] = params[:item][name]
     @session = session
     @item = Item.new(params[:item])
     erb :show
